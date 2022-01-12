58923f4af1f6839540fe0c09bb5ae2951f25bb14
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2022-01-12 17:10:03 +0100

    App converted to functional component and just returning Main component

--- App.js
+++ App.js
@@ -1,23 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Main from './src/components/Main';
 
-export default function App() {
-	return (
-		<View style={styles.container}>
-			<Text>Hello React Native</Text>
-			<StatusBar style='auto' />
-		</View>
-	);
-}
+const App = () => {
+	return <Main />;
+};
 
-console.log('hello there');
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-});
+export default App;
