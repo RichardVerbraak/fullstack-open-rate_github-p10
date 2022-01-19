0c0e640e623fc425e36585699aa201fd9bfd89b4
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2022-01-19 17:12:16 +0100

    Added ScrollView, the spacing might not be exact but it looks close enough to the provided example

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -4,30 +4,42 @@ import Constants from 'expo-constants';
 
 import theme from '../theme.js';
 import AppBarTab from './AppBarTab.jsx';
+import { ScrollView } from 'react-native';
 
 const styles = StyleSheet.create({
 	container: {
-		display: 'flex',
-		flexDirection: 'row',
-		justifyContent: 'space-evenly',
-
-		paddingBottom: 15,
-
 		paddingTop: Constants.statusBarHeight,
 		backgroundColor: theme.colors.primary,
+
 		opacity: 0.95,
+	},
+
+	contentContainer: {
+		display: 'flex',
+		flexDirection: 'row',
+
+		paddingVertical: 17.5,
+		paddingHorizontal: 7.5,
 	},
 	text: {
 		fontWeight: 'bold',
+		fontSize: theme.fontSizes.subheading,
 		color: '#fff',
+
+		padding: 12.5,
 	},
 });
 
 const AppBar = () => {
 	return (
 		<View style={styles.container}>
-			<AppBarTab title={'Repositories'} url={'/'} text={styles.text} />
-			<AppBarTab title={'Sign In'} url={'/login'} text={styles.text} />
+			<ScrollView
+				contentContainerStyle={styles.contentContainer}
+				horizontal={true}
+			>
+				<AppBarTab title={'Repositories'} url={'/'} text={styles.text} />
+				<AppBarTab title={'Sign In'} url={'/login'} text={styles.text} />
+			</ScrollView>
 		</View>
 	);
 };
