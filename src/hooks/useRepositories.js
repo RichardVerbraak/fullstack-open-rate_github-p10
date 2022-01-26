4dbbf8aa2fa84e779de8ec85bffbaaccd4a29d46
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2022-01-26 16:37:17 +0100

    Added another source on useQuery usage

--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -3,8 +3,9 @@ import { useQuery } from '@apollo/client';
 import { GET_REPOSITORIES } from '../graphql/queries';
 
 const useRepositories = () => {
-	// No need to wrap the query in a useEffect hook, useQuery is already an "effect"
-	// https://github.com/trojanowski/react-apollo-hooks/issues/158
+	// No need to wrap the query in a useEffect hook, useQuery is already an "effect" meaning it will run on render or state/prop change
+	// When useQuery renders and why -- https://stackoverflow.com/questions/56964838/trying-call-usequery-in-function-with-react-apollo-hooks
+	// On useEffect usage -- https://github.com/trojanowski/react-apollo-hooks/issues/158
 	const { data, loading, error } = useQuery(GET_REPOSITORIES, {
 		fetchPolicy: 'cache-and-network',
 	});
