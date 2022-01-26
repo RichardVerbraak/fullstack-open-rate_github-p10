a03d50f6d9e9b5606d15a8f5463ab41cd2462aa6
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2022-01-26 16:28:10 +0100

    Added comment on useQuery with useEffect

--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -3,6 +3,8 @@ import { useQuery } from '@apollo/client';
 import { GET_REPOSITORIES } from '../graphql/queries';
 
 const useRepositories = () => {
+	// No need to wrap the query in a useEffect hook, useQuery is already an "effect"
+	// https://github.com/trojanowski/react-apollo-hooks/issues/158
 	const { data, loading, error } = useQuery(GET_REPOSITORIES, {
 		fetchPolicy: 'cache-and-network',
 	});
