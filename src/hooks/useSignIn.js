c0b9fc704caca29b209e8d0d8bc4f06c55173a4d
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2022-01-27 19:24:57 +0100

    Installed AsyncStorage (localStorage for react-native apps) and added in a template hook for signing in the user

---
+++ src/hooks/useSignIn.js
@@ -0,0 +1,11 @@
+const useSignIn = () => {
+	const [mutate, result] = useMutation(/* mutation arguments */);
+
+	const signIn = async ({ username, password }) => {
+		// call the mutate function here with the right arguments
+	};
+
+	return [signIn, result];
+};
+
+export default useSignIn;
