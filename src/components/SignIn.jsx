b0026903045161e541c45d36fac4f5ff4b26480d
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2022-01-28 17:32:08 +0100

    Navigate user back to Repositories after successfully signing in

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -4,6 +4,7 @@ import * as yup from 'yup';
 import { Formik } from 'formik';
 import SignInForm from './SignInForm';
 import useSignIn from '../hooks/useSignIn';
+import { useNavigate } from 'react-router-native';
 
 const initialValues = {
 	username: '',
@@ -18,13 +19,14 @@ const validationSchema = yup.object().shape({
 // handleSubmit is a prop that comes from Formik which calls onSubmit after it's done
 const SignIn = () => {
 	const [signIn] = useSignIn();
+	const navigate = useNavigate();
 
 	const onSubmit = async (values) => {
 		const { username, password } = values;
 
 		try {
-			const data = await signIn({ username, password });
-			console.log(data);
+			await signIn({ username, password });
+			navigate('/', { replace: true });
 		} catch (error) {
 			console.log(error);
 		}
