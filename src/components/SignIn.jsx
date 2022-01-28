9574ea1932784a6807c3a61fe256126653eb6adf
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2022-01-28 17:00:09 +0100

    Added comment and source on the reason for not providing createContext with a default value

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import * as yup from 'yup';
 
 import { Formik } from 'formik';
--- src/contexts/AuthStorageContext.js
+++ src/contexts/AuthStorageContext.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 // Create  global context so components can have access to said context without passing down props
+// This doesn't have an initial value on purpose
+// Explanation: https://kentcdodds.com/blog/how-to-use-react-context-effectively
 const AuthStorageContext = React.createContext();
 
 export default AuthStorageContext;
