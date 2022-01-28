08eccb02279f270910b3ae44f8953d3b48df3368
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2022-01-28 17:28:48 +0100

    Added additional comments on useSignIn hook

--- src/hooks/useSignIn.js
+++ src/hooks/useSignIn.js
@@ -13,9 +13,13 @@ const useSignIn = () => {
 			variables: { credentials: { username, password } },
 		});
 
+		// Stores it in AsyncStorage
 		await authStorage.setAccessToken(data.authenticate.accessToken);
+
+		// Clears Apollo Clients cache and re-executes any active queries
 		apolloClient.resetStore();
 
+		// Return the data to SignIn component to console.log
 		return data;
 	};
 
