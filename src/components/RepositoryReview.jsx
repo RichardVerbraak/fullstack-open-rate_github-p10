9bbb92d84e24ab86703578c30586ab16ae260dc4
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2022-02-07 17:16:10 +0100

    Installed date-fns to format dates to a more readable format

--- src/components/RepositoryReview.jsx
+++ src/components/RepositoryReview.jsx
@@ -3,6 +3,8 @@ import { StyleSheet, View } from 'react-native';
 import theme from '../theme';
 import Text from './Text';
 
+import { format, parseISO } from 'date-fns';
+
 const styles = StyleSheet.create({
 	itemContainer: {
 		backgroundColor: '#fff',
@@ -55,7 +57,9 @@ const RepositoryReview = ({ review }) => {
 				<View style={styles.textContainer}>
 					<View style={styles.textHeading}>
 						<Text fontWeight={'bold'}>{review.user.username}</Text>
-						<Text color={'textSecondary'}>{review.createdAt}</Text>
+						<Text color={'textSecondary'}>
+							{format(parseISO(review.createdAt), 'dd.MM.yyy')}
+						</Text>
 					</View>
 					<Text>{review.text}</Text>
 				</View>
