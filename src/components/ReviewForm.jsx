1b0994cbb56b9c7ecb57b021dd7bc3b8ef1a21c5
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2022-02-07 19:44:06 +0100

    Rough draft of the ReviewForm

---
+++ src/components/ReviewForm.jsx
@@ -0,0 +1,90 @@
+import { StyleSheet, View, Pressable, Text } from 'react-native';
+import React from 'react';
+
+import { Formik } from 'formik';
+import FormikTextInput from './FormikTextInput';
+
+import theme from '../theme';
+
+const styles = StyleSheet.create({
+	formContainer: {
+		display: 'flex',
+		justifyContent: 'space-between',
+
+		height: '40%',
+		width: '90%',
+
+		marginLeft: 'auto',
+		marginRight: 'auto',
+		padding: 20,
+	},
+
+	input: {
+		paddingHorizontal: 20,
+		paddingVertical: 10,
+
+		borderColor: theme.colors.textSecondary,
+		borderStyle: 'solid',
+		borderWidth: 0.5,
+		borderRadius: 5,
+	},
+
+	signIn: {
+		backgroundColor: theme.colors.secondary,
+		color: '#fff',
+		fontWeight: theme.fontWeights.bold,
+		textAlign: 'center',
+
+		paddingHorizontal: 20,
+		paddingVertical: 10,
+
+		borderRadius: 5,
+	},
+});
+
+const initialValues = {
+	username: '',
+	name: '',
+	rating: '',
+	review: '',
+};
+
+const ReviewForm = () => {
+	return (
+		<View>
+			<Formik initialValues={initialValues}>
+				{({ handleSubmit }) => {
+					return (
+						<View onSubmit={handleSubmit} style={styles.formContainer}>
+							<FormikTextInput
+								name='username'
+								placeholder='Repository Owner Name'
+								style={styles.input}
+							/>
+							<FormikTextInput
+								name='name'
+								placeholder='Repository Name'
+								style={styles.input}
+							/>
+							<FormikTextInput
+								name='rating'
+								placeholder='Rating between 0 and 100'
+								style={styles.input}
+							/>
+							<FormikTextInput
+								name='review'
+								placeholder='Review'
+								style={styles.input}
+							/>
+							<Pressable onPress={handleSubmit}>
+								<Text style={styles.signIn}>Create a Review</Text>
+							</Pressable>
+						</View>
+					);
+				}}
+			</Formik>
+		</View>
+	);
+};
+
+export default ReviewForm;
