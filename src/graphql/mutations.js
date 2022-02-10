88a627d8e0b41a3bf7db45360f2ddb2802b43561
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2022-02-10 16:13:25 +0100

    Created SignUp mutation

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -1,5 +1,7 @@
 import { gql } from '@apollo/client';
 
+// @desc		Login user
+// @access		Public
 const SIGN_IN = gql`
 	mutation SignIn($credentials: AuthenticateInput) {
 		authenticate(credentials: $credentials) {
@@ -8,6 +10,18 @@ const SIGN_IN = gql`
 	}
 `;
 
+// @desc		Register user
+// @access		Public
+const SIGN_UP = gql`
+	mutation CreateUser($user: CreateUserInput) {
+		createUser(user: $user) {
+			id
+		}
+	}
+`;
+
+// @desc		Create review
+// @access		Private
 const CREATE_REVIEW = gql`
 	mutation CreateReview($review: CreateReviewInput) {
 		createReview(review: $review) {
@@ -16,4 +30,4 @@ const CREATE_REVIEW = gql`
 	}
 `;
 
-export { SIGN_IN, CREATE_REVIEW };
+export { SIGN_IN, SIGN_UP, CREATE_REVIEW };
